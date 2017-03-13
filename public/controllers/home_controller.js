4718bc2d95107cefd399504784e417f47c3ee147
Koty Wong <wongkoty@gmail.com>
2017-03-13 16:08:43 -0400

    adding lists to screen"

--- public/controllers/home_controller.js
+++ public/controllers/home_controller.js
@@ -1,8 +1,20 @@
 function HomeController($scope, $http) {
   var self = this;
+  var server = 'http://localhost:3000'
+
+  this.lists = ""
+
   this.test = test;
 
   function test() {
     console.log('test')
+    $http.get(`${server}/lists`)
+      .then(response => {
+        console.log(response);
+        self.lists = response.data;
+        // console.log(self.meow);
+      })
   }
+
+  test();
 }
