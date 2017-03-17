60cf12ac05b77cabd449fd1c4788d072ab83d0ea
Koty Wong <wongkoty@gmail.com>
2017-03-17 11:35:28 -0400

    edit todo, show todo

--- public/controllers/todo_controller.js
+++ public/controllers/todo_controller.js
@@ -3,11 +3,38 @@ function TodoController($scope, $http, $state, $stateParams) {
   const self = this;
   const server = 'http://localhost:3000';
 
+  this.todo = "";
+  this.text = "";
 
-
+  this.getTodo = getTodo;
+  this.editTodo = editTodo;
   this.deleteTodo = deleteTodo;
 
+  // State changers
   this.goTodo = goTodo;
+
+  function getTodo(){
+    console.log($stateParams.todoId)
+    $http.get(`${server}/todos/${$stateParams.todoId}`)
+      .then(function(response){
+        console.log(response);
+        self.todo = response.data;
+      })
+  }
+
+  function editTodo() {
+    console.log('edit todo');
+    const data = { text: self.text }
+    $http.put(`${server}/todos/${$stateParams.todoId}`, data)
+      .then(response => {
+        console.log(response);
+        self.title = "";
+        if (response.status == 200) {
+          console.log('changing list')
+          self.todo = response.data;
+        }
+      })
+  }
 
   function deleteTodo(todoId) {
     console.log('deleteTodo')
